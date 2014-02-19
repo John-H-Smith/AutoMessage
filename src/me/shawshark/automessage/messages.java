d72f97f97541994d042eb90f8f64f0a7e6377ffe
Chris <chrissbolding@gmail.com>
2014-02-20 01:02:55 +1100

    Create messages.java

---
+++ src/me/shawshark/automessage/messages.java
@@ -0,0 +1,13 @@
+package me.shawshark.automessage;
+
+public class messages {
+	
+	public String message;
+	public int id;
+	
+	public messages(String imessage, int iid) {
+		message = imessage;
+		id = iid;
+	}
+
+}
